6af6f46637fc3708da00ba0021a1fda58727f49b
Miguel Campos <camposmiguel@gmail.com>
2016-10-13 12:25:19 +0200

    CircleImageView, Drawable XML

---
+++ 05_ExternalLibraries/01_CircleImageView/app/src/main/java/com/miguelcr/a01_circleimageview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.miguelcr.a01_circleimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}

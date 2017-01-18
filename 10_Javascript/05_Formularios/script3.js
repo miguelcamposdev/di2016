86c8cb9c42f296bbf145ed958e6c02b2c3752022
Miguel Campos <camposmiguel@gmail.com>
2017-01-18 09:53:24 +0100

    Javascript index3

---
+++ 10_Javascript/05_Formularios/script3.js
@@ -0,0 +1,26 @@
+
+function restarProducto(idProducto) { // "restarProducto-1"
+    var arrayIdProducto = idProducto.split("-");
+    // arrayIdProducto[0] > restarProducto
+    // arrayIdProducto[1] > 1
+    var numeroFila = arrayIdProducto[1];
+    var spanCantidadProducto = document.getElementById("cantidadProducto-"+numeroFila);
+
+    var cantidadProductoActual = spanCantidadProducto.innerHTML;
+    var precioUnitarioProducto = spanCantidadProducto.getAttribute("precio");
+
+    var cantidadProductoNueva = parseInt(cantidadProductoActual) - 1;
+
+    // Actualizo la cantidad de elementos del producto
+    spanCantidadProducto.innerHTML = cantidadProductoNueva;
+
+    // Actualizo el precio totalizado del producto
+    ...
+
+}
+
+// Este método se lanza cuando la
+window.onload = function() {
+  document.getElementById("restarProducto-1").onclick = restarProducto("restarProducto-1");
+  document.getElementById("restarProducto-2").onclick = restarProducto("restarProducto-2");
+}
